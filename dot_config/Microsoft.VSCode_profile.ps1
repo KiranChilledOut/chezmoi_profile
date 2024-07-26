efb9bd593063bf5539ccf12b1f2ef359feaaf69a
Kiran Raj Rajendran <krajendran@tesla.com>
2024-07-26 13:24:45 +0200

    adding choco

--- dot_config/Microsoft.VSCode_profile.ps1
+++ dot_config/Microsoft.VSCode_profile.ps1
@@ -1,30 +1,43 @@
+
+# Check if Chocolatey is installed
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    Write-Host "Chocolatey is not installed. Installing Chocolatey..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+    Write-Host "Chocolatey is already installed."
+}
+
+# Check if PSReadLine is installed
+if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
+    Write-Host GuiCompletion is not installed. Installing PSReadLine..."
+    choco install GuiCompletion -y
+} else {
+    Write-Host GuiCompletion is already installed."
+}
+
+# Check if PSReadLine is installed
+if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
+    Write-Host "PSReadLine is not installed. Installing PSReadLine..."
+    choco install psreadline -y
+} else {
+    Write-Host "PSReadLine is already installed."
+}
+
+# Check if Starship is installed
+if (-not (Get-Command starship -ErrorAction SilentlyContinue)) {
+    Write-Host "Starship is not installed. Installing Starship..."
+    choco install starship -y
+} else {
+    Write-Host "Starship is already installed."
+}
+
+# Load Starship prompt
 Invoke-Expression (&starship init powershell)
+
+# Additional profile customizations
+Import-Module PSReadLine
+
 Install-GuiCompletion -Key Tab
 
-<#
-function Prompt
-{
-    Write-Host "[" -NoNewline
-    Write-Host (Get-Date -Format "HH:mm:ss") -ForegroundColor Gray -NoNewline
-    
-    try
-    {
-        $history = Get-History -ErrorAction Ignore
-        if ($history)
-        {
-            Write-Host "][" -NoNewline
-            if (([System.Management.Automation.PSTypeName]'Sqlcollaborative.Dbatools.Utility.DbaTimeSpanPretty').Type)
-            {
-                Write-Host ([Sqlcollaborative.Dbatools.Utility.DbaTimeSpanPretty]($history[-1].EndExecutionTime - $history[-1].StartExecutionTime)) -ForegroundColor Gray -NoNewline
-            }
-            else
-            {
-                Write-Host ($history[-1].EndExecutionTime - $history[-1].StartExecutionTime) -ForegroundColor Gray -NoNewline
-            }
-        }
-    }
-    catch { }
-    Write-Host "] $($executionContext.SessionState.Path.CurrentLocation.ProviderPath)" -NoNewline
-    "> "
-}
-#>
