e6a38e1b2f19f02d2fa35c867fcfc554d156a396
Kiran Raj Rajendran <krajendran@tesla.com>
2024-07-26 12:47:11 +0200

    initial commit

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,32 @@
+﻿Invoke-Expression (&starship init powershell)
+Install-GuiCompletion -Key Tab
+
+<#
+function Prompt
+{
+    Write-Host "[" -NoNewline
+    Write-Host (Get-Date -Format "HH:mm:ss") -ForegroundColor Gray -NoNewline
+    
+    try
+    {
+        $history = Get-History -ErrorAction Ignore
+        if ($history)
+        {
+            Write-Host "][" -NoNewline
+            if (([System.Management.Automation.PSTypeName]'Sqlcollaborative.Dbatools.Utility.DbaTimeSpanPretty').Type)
+            {
+                Write-Host ([Sqlcollaborative.Dbatools.Utility.DbaTimeSpanPretty]($history[-1].EndExecutionTime - $history[-1].StartExecutionTime)) -ForegroundColor Gray -NoNewline
+            }
+            else
+            {
+                Write-Host ($history[-1].EndExecutionTime - $history[-1].StartExecutionTime) -ForegroundColor Gray -NoNewline
+            }
+        }
+    }
+    catch { }
+    Write-Host "] $($executionContext.SessionState.Path.CurrentLocation.ProviderPath)" -NoNewline
+    "> "
+}
+#>
+
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
